497fbc111c9b7db5e9ef3c70f96bedb7e4bcb35f
Mohd Shariq <111305981+ShariqYT@users.noreply.github.com>
2024-07-04 11:22:40 +0530

    Fix: Order-Placed page

--- components/extra/OrderDetails.js
+++ components/extra/OrderDetails.js
@@ -29,7 +29,7 @@ const OrderDetails = () => {
     setTime(d.toLocaleTimeString());
     const fetchOrder = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/api/getOrderID?id=${id}`);
+        const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/getOrderID?id=${id}`);
         const data = await response.json();
         setOrder(data.order);
       } catch (error) {
@@ -53,8 +53,8 @@ const OrderDetails = () => {
           Payment Successful
         </h2>
         <p className="mt-4 font-normal text-lg leading-8 text-gray-500 mb-11 text-center">Thanks for making a purchase you can check your order summary from below</p>
-        <div className="main-box border border-purple-700 rounded-xl pt-6 max-w-xl max-lg:mx-auto lg:max-w-full">
-          <div className="flex flex-col lg:flex-row lg:items-center justify-between px-6 pb-6 border-b border-gray-200">
+        <div className="main-box border border-purple-700 rounded-xl pt-6 max-w-xl max-md:mx-auto md:max-w-full">
+          <div className="flex flex-col md:flex-row md:items-center justify-between px-6 pb-6 border-b border-gray-200">
             <div className="data">
               <p className="font-semibold text-2xl leading-7 text-black">Order Id: <span className="text-purple-700 font-semibold">#{order.oid}</span></p>
               <p className="font-semibold text-base leading-7 text-black mt-4">Order Payment : <span className="text-purple-700 font-medium">{date} | {time}</span></p>
@@ -66,7 +66,7 @@ const OrderDetails = () => {
                   {order.status}
                 </p>
               </div>
-              <button className="rounded-full py-4 px-5 font-semibold text-sm leading-7 text-white bg-purple-700 max-lg:mt-5 shadow-sm shadow-transparent transition-all duration-500 hover:bg-purple-800 hover:shadow-purple-600">
+              <button className="rounded-full py-4 px-5 font-semibold text-sm leading-7 text-white bg-purple-700 max-md:mt-5 shadow-sm shadow-transparent transition-all duration-500 hover:bg-purple-800 hover:shadow-purple-600">
                 Track Your Order
               </button>
             </div>
@@ -74,10 +74,10 @@ const OrderDetails = () => {
           {Object.keys(product).map((key) => {
             return (
               <div key={key} className="w-full px-3 min-[400px]:px-6">
-                <div className="flex flex-col lg:flex-row items-center py-6 border-b border-gray-200 gap-6 w-full">
-                  <div className="img-box max-lg:w-full">
+                <div className="flex flex-col md:flex-row items-center py-6 border-b border-gray-200 gap-6 w-full">
+                  <div className="img-box max-md:w-full">
                     <Image unoptimized quality={100} width={1} height={1} priority={true} src={product[key].img} alt="Premium Watch image"
-                      className="aspect-square w-full lg:max-w-[140px]" />
+                      className="aspect-square w-full md:max-w-[140px]" />
                   </div>
                   <div className="flex flex-row items-center w-full ">
                     <div className="flex justify-between w-full">
@@ -92,9 +92,9 @@ const OrderDetails = () => {
                       </div>
                       <div className="flex">
                         <div className="flex items-center mx-20">
-                          <div className="flex gap-3 lg:block">
+                          <div className="flex gap-3 md:block">
                             <p className="font-medium text-xl leading-7 text-black">Price</p>
-                            <p className="lg:mt-4 font-medium text-xl leading-7 text-purple-700">₹{product[key].price}</p>
+                            <p className="md:mt-4 font-medium text-xl leading-7 text-purple-700">₹{product[key].price}</p>
                           </div>
                         </div>
                       </div>
@@ -104,7 +104,7 @@ const OrderDetails = () => {
               </div>
             );
           })}
-          <div className="w-full border-t border-gray-200 px-6 flex flex-col lg:flex-row items-center justify-end ">
+          <div className="w-full border-t border-gray-200 px-6 flex flex-col md:flex-row items-center justify-end ">
           </div>
           <p className="font-semibold text-right text-lg text-black py-6 px-6">Total Price: <span className="text-2xl text-purple-700">₹{inrTotal}</span></p>
         </div>
