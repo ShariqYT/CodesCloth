3ad2b09fcbc669c844e3228c22f04893da7e1614
Mohd Shariq <111305981+ShariqYT@users.noreply.github.com>
2024-07-08 22:38:44 +0530

    .

--- app/admin/all-orders/page.js
+++ app/admin/all-orders/page.js
@@ -1,10 +1,12 @@
+import { getAllProducts } from '@/actions/Admin/getAllProducts'
 import AllProduct from '@/components/Tables/AllProduct'
 import React from 'react'
 
 const AllOrders = () => {
+  let products = getAllProducts()
   return (
     <>
-      <AllProduct />
+      <AllProduct products={products} />
     </>
   )
 }
