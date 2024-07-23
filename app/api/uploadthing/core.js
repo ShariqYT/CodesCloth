40440f14767dfd19fd248a7f0605fcb18e2d2fc8
Mohd Shariq <111305981+ShariqYT@users.noreply.github.com>
2024-07-23 06:57:01 +0530

    fix: color missing

--- app/api/uploadthing/core.js
+++ app/api/uploadthing/core.js
@@ -3,7 +3,7 @@ import { createUploadthing } from "uploadthing/next";
 const f = createUploadthing();
   
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "512KB", maxFileCount: 1 } })
+  imageUploader: f({ image: { maxFileSize: "128KB", maxFileCount: 1 } })
     
     .onUploadComplete(async ({ metadata, file }) => {
       
--- components/extra/SizeandColor.js
+++ components/extra/SizeandColor.js
@@ -16,6 +16,31 @@ const SizeandColor = ({ product, variants }) => {
 
     return (
         <div className="flex mt-6 md:flex-row flex-col md:items-center pb-5">
+            <div className="
+            hidden
+  bg-red-500
+  bg-pink-500
+  bg-purple-500
+  bg-indigo-500
+  bg-blue-500
+  bg-lightBlue-500
+  bg-cyan-500
+  bg-teal-500
+  bg-green-500
+  bg-lime-500
+  bg-yellow-500
+  bg-amber-500
+  bg-orange-500
+  bg-red-500
+  bg-warmGray-500
+  bg-trueGray-500
+  bg-gray-500
+  bg-coolGray-500
+  bg-blueGray-500
+">
+                Your content here
+            </div>
+
             <div className="flex items-center">
                 <span className="mr-3">Color</span>
                 {colors.map((col) => (
